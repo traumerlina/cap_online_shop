243d3ee1f402a3217b4c13308fbb367eb0a95b02
whois-who <zalkargame0@gmail.com>
2022-11-25 10:50:32 +0600

    ddd

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import TopPart from '../components/topPartHP';
-
+import MainPart from '../components/mainPartHP'
 
 const HomePage = () => {
   return (
     <div>
       <TopPart/>
+      <MainPart/>
     </div>
   )
 }
