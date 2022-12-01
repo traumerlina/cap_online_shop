f080109081dc5780eaf209237ff5c19bbb928d26
bekbolotahahah <112771389+bekbolotahahah@users.noreply.github.com>
2022-12-01 23:30:36 +0600

    [cart and modal ]

--- src/pages/cartPage.js
+++ src/pages/cartPage.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import Footer from '../components/footer';
+import CartPart from '../components/cartPart/cartPart';
+
+
 const CartPage = () => {
   return (
     <div>
+      <CartPart/>
       
       <Footer/>
+
     </div>
   )
 }
 
-export default CartPage;
+export default CartPage;  
