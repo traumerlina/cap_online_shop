1181070676a65e05ea5cdf7797f0b459a9d7c5a9
ulugbekpermatov <d9911833@gmail.com>
2022-12-25 15:28:19 +0600

    ulug

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -3,14 +3,19 @@ import TopPart from '../components/topPartHP';
 import MainPart from '../components/mainPartHP'
 import BottomPartHP from '../components/bottomPartHP';
 import Footer from '../components/footer';
+import ScrollCheck from '../components/scrollCheck';
 
 const HomePage = () => {
   return (
     <div>
       <TopPart/>
-      <MainPart/>
-      <BottomPartHP/>
-      <Footer/>
+      {/* <ScrollCheck/> */}
+
+
+        <MainPart/>
+        <BottomPartHP/>
+        <Footer/>
+
     </div>
   )
 }
