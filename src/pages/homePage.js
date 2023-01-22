749a42718d2784658e2ce7018308006473c204c7
jnshbkv01 <erbol.janyshbekov@mail.ru>
2023-01-22 11:04:42 +0600

    er0

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -5,12 +5,14 @@ import BottomPartHP from '../components/bottomPartHP';
 import Footer from '../components/footer';
 import ScrollCheck from '../components/scrollCheck';
 import TopSlider from '../components/topSlider';
+import ExtraMenu from '../components/extraMenu';
 
 const HomePage = () => {
   return (
     <div>
       <TopPart/>
       <TopSlider/>
+      <ExtraMenu/>
       {/* <ScrollCheck/> */}
 
         <MainPart/>
